143a61dbf5da242dee8b87a42a2f709ac11d2a22
hei hei OK OK! <98307235+laisammy@users.noreply.github.com>
2026-03-23 20:53:44 +0000

    Added responsive design to Hero page

--- src/hero.jsx
+++ src/hero.jsx
@@ -9,56 +9,59 @@ import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 export default function Hero() {
     return(
         <ScrollFadeIn>
-            <div className="pattern-background flex flex-row items-center justify-center h-screen">
-                <div id="left" className="bg-background text-text lexend flex flex-col justify-centerp-12 p-8 h-100">
-                    <h1 className="text-5xl font-semibold tracking-[0.07em]">JACOB WONG</h1>
-                    <p className="font-normal mt-2 tracking-wider">
+            <div className="pattern-background min-h-screen flex items-center justify-center ">
+                <div className="flex flex-col md:flex-row w-full max-w-6xl items-stretch lg:scale-75">
+                    <div className="bg-background text-text lexend flex flex-col justify-center p-9 w-full">
+                        <h1 className="text-[clamp(2.5rem,5vw,4.5rem)] font-semibold tracking-[0.07em]">
+                        JACOB WONG
+                        </h1>
+
+                        <p className="font-normal mt-2 tracking-wider text-[clamp(1.3rem,2.5vw,2rem)]">
                         Actor / Musician
-                    </p>
-                    <p className="mt-6 max-w-md font-light">
+                        </p>
+
+                        <p className="mt-6 font-light max-w-md text-[clamp(1rem,1.8vw,1.3rem)]">
                         A musical theatre performer from Hong Kong, he has originated roles in both professional and amateur productions and has taught at numerous institutions across the city. 
                         He is now excited to continue his artistic and professional development overseas at RWCMD.
-                    </p>
+                        </p>
 
-                    <div className="flex flex-row gap-3 mt-6">
-                        <div className="flex flex-row gap-6">
-                            <a href="https://www.instagram.com/jacobwch" className="flex items-center g-2 hover:text-textHover">
-                                <span>
-                                    Instagram
-                                </span>
-                                <FontAwesomeIcon icon={faInstagram} className="text-2xl" />
-                            </a>
-                        </div>
-                        <div className="flex flex-row gap-3">
-                            <a href="https://www.linkedin.com/in/jacob-wong-012865324/" className="flex items-center g-2 hover:text-textHover">
-                                <span>
-                                    Linkedin
-                                </span>
-                                <FontAwesomeIcon icon={faLinkedin} className="text-2xl" />
-                            </a>
-                        </div>
-                    </div>
+                        <div className="flex flex-wrap gap-6 mt-6 text-[clamp(1rem,1.8vw,1.3rem)]">
+                        <a href="https://www.instagram.com/jacobwch" className="flex items-center gap-2 hover:text-textHover">
+                            <span>Instagram</span>
+                            <FontAwesomeIcon icon={faInstagram} className="text-2xl" />
+                        </a>
 
-                    <div id = "Navigation" className="mt-7 flex flex-row gap-3">
-                        <Link to="/" className="outline-solid outline-1 outline-text/75 p-2 font-sans tracking-widest hover:bg-hover">
+                        <a href="https://www.linkedin.com/in/jacob-wong-012865324/" className="flex items-center gap-2 hover:text-textHover">
+                            <span>LinkedIn</span>
+                            <FontAwesomeIcon icon={faLinkedin} className="text-2xl" />
+                        </a>
+                        </div>
+
+                        <div id="Navigation" className="text-[clamp(1rem,1.4vw,1.2rem)] mt-7 flex flex-wrap gap-3">
+                        <Link to="/" className="outline-1 outline-text/75 p-2 tracking-widest hover:bg-hover">
                             HOME
                         </Link>
-                        <Link to="/about" className="outline-solid outline-1 outline-text/75 p-2 font-sans tracking-widest hover:bg-hover">
+                        <Link to="/about" className="outline-1 outline-text/75 p-2 tracking-widest hover:bg-hover">
                             ABOUT
                         </Link>
-                        <Link to="/gallery" className="outline-solid outline-1 outline-text/75 p-2 font-sans tracking-widest hover:bg-hover">
+                        <Link to="/gallery" className="outline-1 outline-text/75 p-2 tracking-widest hover:bg-hover">
                             GALLERY
                         </Link>
-                        <Link to="/resume" className="outline-solid outline-1 outline-text/75 p-2 font-sans tracking-widest hover:bg-hover">
+                        <Link to="/resume" className="outline-1 outline-text/75 p-2 tracking-widest hover:bg-hover">
                             RESUME
                         </Link>
-                        <Link to="/contact" className="outline-solid outline-1 outline-text/75 p-2 font-sans tracking-widest hover:bg-hover">
+                        <Link to="/contact" className="outline-1 outline-text/75 p-2 tracking-widest hover:bg-hover">
                             CONTACT
                         </Link>
+                        </div>
                     </div>
-                </div>
-                <div id="right" className="bg-primary flex items-center justify-center" >
-                    <img src="/heroHeadshot.JPG" className="h-100"></img>
+
+                    <div className="flex items-center justify-center w-full">
+                    <img
+                        src="/heroHeadshot.JPG"
+                        className="w-full h-full object-cover"
+                    />
+                    </div>
                 </div>
             </div>
         </ScrollFadeIn>
